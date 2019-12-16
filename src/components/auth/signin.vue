cc0ba089d14a11e0fd7f53406491879cb1219fd5
adeojoemmanuel <emmanuel.adeojo@yahoo.com>
2019-12-16 05:38:00 +0100

    added login page

---
+++ src/components/auth/signin.vue
@@ -0,0 +1,115 @@
+<template>
+  <!--start-->
+  <section class="main-content-reg-tx" style="padding: 120px 0px;">
+    <div class="container">
+      <div class="row">
+        <div class="col-md-2"></div>
+        <div class="col-md-8">
+          <div class="form-box" style="border: none; padding: 40px;">
+            <div class="row">
+              <div class="col-sm-12">
+                <form action="" class="form-horizontal">
+                  <div class="form-group form-group-tx row">
+                    <label for="" class="col-md-3 control-label">
+                      <!-- You are a <sup>*</sup> -->
+                    </label>
+                    <div class="col-md-6">
+                      <div class="row">
+                        <div class="col-md-6">
+                          <div class="social-media-button">
+                            <button class="btn btn-block btn-reg facebook-reg">
+                              <i class="fa fa-facebook fa-2x"></i> Login
+                            </button>
+                          </div>
+                        </div>
+                        <div class="col-md-6">
+                          <div class="social-media-button">
+                            <button class="btn btn-block btn-reg google-reg">
+                              <i class="fa fa-google-plus fa-2x"></i> Login
+                            </button>
+                          </div>
+                        </div>
+                      </div>
+                    </div>
+                  </div>
+                  <div class="row">
+                    <div class="col-md-3"></div>
+                    <div class="col-md-6">
+                      <div class="bc-auth-card__form-separator"></div>
+                    </div>
+                  </div>
+
+                  <div class="form-group form-group-tx row">
+                    <div class="col-md-3"></div>
+                    <div class="col-md-6">
+                      <label for="" class="control-label">
+                        Name
+                      </label>
+                      <div class="radio">
+                        <input
+                          name=""
+                          placeholder="First Name"
+                          class="form-control input-md"
+                          required=""
+                          type="text"
+                        />
+                      </div>
+                    </div>
+                  </div>
+
+                  <div class="form-group form-group-tx row">
+                    <div class="col-md-3"></div>
+                    <div class="col-md-6">
+                      <label for="" class="control-label">
+                        Password
+                      </label>
+                      <div class="radio">
+                        <input
+                          name=""
+                          placeholder="Password"
+                          class="form-control input-md"
+                          required=""
+                          type="password"
+                        />
+                        <!-- <small id="passwordHelpBlock" class="form-text text-muted">At least 5 characters</small> -->
+                      </div>
+                    </div>
+                  </div>
+                  <div class="form-group form-group-tx row">
+                    <label class="col-md-3 control-label"></label>
+
+                    <div class="col-md-8">
+                      <div style="clear:both"></div>
+                      <div class="row">
+                        <div class="col-md-9">
+                          <a
+                            class="btn btn-block btn-primary btn-register-tx"
+                            href="account-home.html"
+                            >Register</a
+                          >
+                        </div>
+                      </div>
+                    </div>
+                  </div>
+                </form>
+              </div>
+            </div>
+          </div>
+        </div>
+        <div class="col-md-2"></div>
+      </div>
+    </div>
+  </section>
+  <!--/ Main-content -->
+  <!--Footer Section-->
+
+  <!--/stop -->
+</template>
+
+<script>
+export default {
+  name: "login"
+};
+</script>
+
+<style></style>
